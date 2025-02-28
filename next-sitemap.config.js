45563a9b1ab6baf63dd3a0cc138f78c3d57f2026
Sandeep1816 <sandeepavala2@gmail.com>
2025-02-28 12:50:08 +0530

    Normalize line endings

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://www.futuredigitechsummit.com/", // ✅ Replace with your actual domain
+    generateRobotsTxt: true, // ✅ Automatically generate robots.txt
+    sitemapSize: 5000, // ✅ Limit URLs per sitemap file
+  };
+  
